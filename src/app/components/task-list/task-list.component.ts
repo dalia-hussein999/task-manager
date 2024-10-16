84b97df6da0eab627c4f78e17932d287338e3872
Mohamed Ramadan <daliahussein999@gmail.com>
2024-10-16 14:21:38 +0300

    fetch data

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FetchTasksService } from '../../services/fetch-tasks.service';
+import { Task } from '../../interfaces/task';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
+  errorMessage: string | null = null; 
 
+  constructor(private fetchTasksService: FetchTasksService) {}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
+    this.fetchTasksService.getTasks()
+      .pipe(
+        catchError((error) => {
+          this.errorMessage = "Error loading tasks."; 
+          console.error("Error loading tasks:", error);
+          return of([]); 
+        })
+      )
+      .subscribe((data) => {
+        this.tasks = data;
+        console.log("tasks:", this.tasks);
+      });
+  }
+
+  toggleCompletion(task: Task): void {
+    task.completed = !task.completed;
+  }
 }
--- src/app/services/fetch-tasks.service.ts
+++ src/app/services/fetch-tasks.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../interfaces/task';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FetchTasksService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
 }
